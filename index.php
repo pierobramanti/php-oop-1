e7e6a9e04ae86fddd4790b535565c3a626ae0a12
pierobramanti <piero.bramanti94@gmail.com>
2024-09-17 15:39:36 +0200

    [UPD] Added constructor to Movie class

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <?php
 
-//
-class Movie{
+// Classe Movie
+class Movie {
 
+    // Variabili d'istanza
     public $title;
     public $director;
     public $genre;
 
+    // Costruttore
+    public function __construct($_title, $_director, $_genre) {
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->genre = $_genre;
+    }
+
 }
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
