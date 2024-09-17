ccdb53cabbab87611e5b21c5384dff83a5f9b3d1
pierobramanti <piero.bramanti94@gmail.com>
2024-09-17 15:33:18 +0200

    [UPD] created index.php and created class Movie

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+//
+class Movie{
+
+    public $title;
+    public $director;
+    public $genre;
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
